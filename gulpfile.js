f9302c248c503cea356e032cd0ea201e410046f6
Brandon Sandall <44983311+BrandonSandall@users.noreply.github.com>
2019-08-02 12:15:11 -0700

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -43,22 +43,7 @@ gulp.task('vendor', function() {
 
 });
 
-// Compile SCSS
-/*gulp.task('css:compile', function() {
-  return gulp.src('./scss/**/*.scss')
-    .pipe(sass.sync({
-      outputStyle: 'expanded'
-    }).on('error', sass.logError))
-    .pipe(autoprefixer({
-      browsers: ['last 2 versions'],
-      cascade: false
-    }))
-    .pipe(header(banner, {
-      pkg: pkg
-    }))
-    .pipe(gulp.dest('./css'))
-});
-*/
+
 
 // Minify CSS
 gulp.task('css:minify', ['css:compile'], function() {
